254f3305402295946e0165f915eddc486567e010
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-16 16:47:42 +0100

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250) NOT NULL,
+  date_of_birth date NOT NULL,
+  escape_attempts INT NOT NULL,
+  neutered boolean NOT NULL,
+  weight_kg decimal NOT NULL,
+  PRIMARY KEY(id)
 );
