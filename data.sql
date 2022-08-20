b4c03dce274f258327e86f6f3933d3db4633ac44
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-20 03:33:19 +0100

    Insert data for vets

--- data.sql
+++ data.sql
@@ -41,3 +41,13 @@ UPDATE animals SET owner_id = 5 WHERE name='Angemon' OR name='Boarmon';
 -- id 3 = Bob owns Devimon and Plantmon.
 -- id 4 = Melody Pond owns Charmander, Squirtle, and Blossom.
 -- id 5 = Dean Winchester owns Angemon and Boarmon.
+
+
+/* PROJECT DAY 4 */
+
+/* INSERT DATA FOR VETS */
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, '2019-01-17');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, '1981-05-04');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, '2008-06-08');
+
